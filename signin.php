ad4c4a2457eba197743a5edac53f9ed64e74a72b
Manideepthi07 <49177774+Manideepthi07@users.noreply.github.com>
2019-04-02 10:13:42 +0530

    Update signin.php

--- signin.php
+++ signin.php
@@ -3,17 +3,22 @@ $UserName=$_POST['uname'];
 $PhoneNumber=$_POST['phno'];
 $UserName=stripcslashes($UserName);
 $PhoneNumber=stripcslashes($PhoneNumber);
-$UserName=mysql_real_escape_string($UserName);
-$PhoneNumber=mysql_real_escape_string($PhoneNumber);
-mysql_connect("localhost","root","");
-mysql_select_db("digifarming");
-$result=mysql_query("select * from farmers where UserName='$UserName' and PhoneNumber='$PhoneNumber'") or
- die("Failed to query database".mysql_error());
-$row=mysql_fetch_array($result);
+$UserName=db2_escape_string($UserName);
+$PhoneNumber=db2_escape_string($PhoneNumber);
+$database="BLUDB";
+$user="dlf90618";
+$password="67dxjg7x2t-97hl5";
+$conn=db2_connect($database,$user,$password);
+if(!$conn){
+	echo "Connection failed";
+}
+$result=db2_exec($conn,"select * from FARMERS where USERNAME='$UserName' and PHONENUMBER='$PhoneNumber'");
+while($row=db2_fetch_array($result)){
 if($row['UserName']==$UserName && $row['PhoneNumber']==$PhoneNumber){
 	echo "<script>window.open('details.html','_self')</script>";
 }
 else{
 	echo "Email or password incorrect";
+}
 }
 ?>
