13f61a00d5b98f9c1cd18c55c9f763a5bc43e4ed
Manideepthi07 <49177774+Manideepthi07@users.noreply.github.com>
2019-04-02 10:18:25 +0530

    Update maincode.php

--- maincode.php
+++ maincode.php
@@ -1,17 +1,21 @@
 <?php
 $cropname=isset($_POST["cropname"]) ?$_POST["cropname"]:" ";
-$cropname=mysql_real_escape_string($cropname);
-mysql_connect("localhost","root","") or die("Unable to connect to database".mysql_error());
-mysql_select_db("digifarming");
-$q1="select cropdetails.Name,previousdatabase.State,previousdatabase.PricePerTonne,cropdetails.Quantity,cropdetails.Quantity*previousdatabase.PricePerTonne from cropdetails INNER JOIN previousdatabase on cropdetails.Name=previousdatabase.CropName";
-$result= mysql_query($q1) or die("Unable to query the data".mysql_error());
+$cropname=db2_escape_string($cropname);
+$database="BLUDB";
+$user="dlf90618";
+$password="67dxjg7x2t-97hl5";
+$conn=db2_connect($database,$user,$password);
+if(!$conn){
+	echo "Connection failed";
+}
+$result=db2_exec($conn,"select cropdetails.Name,previousdatabase.State,previousdatabase.PricePerTonne,cropdetails.Quantity,cropdetails.Quantity*previousdatabase.PricePerTonne from cropdetails INNER JOIN previousdatabase on cropdetails.Name=previousdatabase.CropName");
 echo "<table border=1>";
 echo "<th>CropName</th>";
 echo "<th>State</th>";
 echo "<th>PricePerTonne</th>";
 echo "<th>Quantity</th>";
 echo "<th>TotalPrice</th>";
-while($row=mysql_fetch_array($result,MYSQL_NUM)){
+while($row=db2_fetch_array($result,MYSQL_NUM)){
  echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" . $row[3] . "</td><td>" .  $row[4] ."</td></tr>";
 }
 echo "</table>";
